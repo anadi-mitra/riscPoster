661871efc57f7fd38c618bffa1d9f2a24d33c988
anadi <anadimitra777@gmail.com>
2025-03-24 14:18:34 +0530

    updated examples

--- src/code/Escape.java
+++ src/code/Escape.java
@@ -3,22 +3,18 @@ static Point g1;   //some global variable
 void foo(MyClass c){
 
     Point p1= new Point(32,6);  //O1
-
     Point p2= new Point(16,4);  //O2
-
     Point p3= new Point(8,3);   //O3
 
     //passing O1 and O2 to bar
     bar(p1,p2);
-
     //we don't have implementation of foobar at static time
-    c.foobar(p3);
-
+    c.foobar(p3);   //O3 escapes or not?
 }
 
 void bar(Point q1, Point q2){
 
-    //displaying fields of q1. not escaping
+    //only displaying fields of q1 hence O1 does not escape
     System.out.println("("+q1.x+","+q1.y+")");
 
     g1=q2;  //p2 is escaping to global space via g1
--- src/code/PolyglotExample.java
+++ src/code/PolyglotExample.java
@@ -1,15 +1,15 @@
 void java-python(String anotherScript) {
-
+    //API to manage polyglot environment
     Context context = Context.newBuilder().build();
 
     Point generatorPoint= new Point(32,54);//cartesian point
 
     String pythonScript =
         "def pointDoubling(point):\n" +
-                "    slope= (3* point.x* point.x- 3)/(2* point.y)\n" +
-                "    x= slope*slope-2*point.x\n" +
-                "    y= slope*(point.x-x)-point.y\n" +
-                "    return Point.newPoint(x, y)\n"; //#object created
+                "\tslope= (3* point.x* point.x- 3)/(2* point.y)\n" +
+                "\tx= slope*slope-2*point.x\n" +
+                "\ty= slope*(point.x-x)-point.y\n" +
+                "\treturn Point.newPoint(x, y)\n"; //object created
 
     //context is API for managing polyglot runtime
     context.eval(Source.newBuilder("python", pythonScript).build());
